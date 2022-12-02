45ea3699101a66a3e83ac5cd8d2129f7ddd36306
Pushkar Borkar <54065262+Pushkar1809@users.noreply.github.com>
2022-12-03 02:05:38 +0530

    Add Button Component

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -3,16 +3,18 @@ import { Button, Icon, Text } from '@chakra-ui/react';
 interface Props {
   icon: any;
   prompt: string;
+  color: string;
+  bg: string;
 }
 
-const PrimaryButton = ({ icon, prompt }: Props) => {
+const PrimaryButton = ({ icon, prompt, color, bg }: Props) => {
   return (
     <Button
-      color={'white'}
-      backgroundColor={'black'}
+      color={color}
+      backgroundColor={bg}
       rightIcon={<Icon as={icon} />}
       borderRadius={'full'}
-      _hover={{ background: 'black' }}
+      _hover={{ background: bg }}
     >
       {prompt}
     </Button>
